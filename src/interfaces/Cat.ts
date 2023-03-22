b87e5acdb6ec31e9aaec7969182c4080f617dd8f
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2023-03-22 14:35:20 +0200

    fix with prettier

--- src/interfaces/Cat.ts
+++ src/interfaces/Cat.ts
@@ -1,1 +1,13 @@
+import {Point} from 'geojson';
+import {Document} from 'mongoose';
+import {User} from './User';
+
 // TODO: cat interface
+interface Cat extends Document {
+  cat_name: string;
+  owner: User | string;
+  weight: number;
+  birthdate: Date;
+  cords: Point;
+}
+export {Cat};
--- src/interfaces/User.ts
+++ src/interfaces/User.ts
@@ -1,1 +1,17 @@
 // TODO: user interface
+import {Document} from 'mongoose';
+
+interface User extends Document {
+  user_name: string;
+  email: string;
+  password: string;
+  role: 'user' | 'admin';
+}
+
+interface UserOutput {
+  _id: string;
+  user_name: string;
+  email: string;
+}
+
+export {User, UserOutput};
